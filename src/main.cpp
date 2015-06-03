14e00a4e2a0d699536d4eca14bacc25d4cf42b1b
Dan Nilsson <raxptor@gmail.com>
2015-06-03 16:52:09 +0200

    - stuffs

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <kosmos/render/render.h>
 #include <kosmos/render/render2d.h>
 #include <kosmos/render/shader.h>
+#include <kosmos/core/mat4.h>
 #include <kosmos/log/log.h>
 
 #include <cassert>
@@ -39,6 +40,35 @@ void frame(laxion::appwindow::input_batch *input, float deltatime)
 	laxion::appwindow::get_client_rect(window, &x0, &y0, &x1, &y1);
 	
 	kosmos::render::begin(x1-x0, y1-x0, true, true, 0xff00ff);
+	
+	
+	float persp[16];
+	kosmos::mat4_persp(persp, 0.7f, 0.3f, 1.0f, 100.0f);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf(persp);
+	
+	glLineWidth(2.5); 
+	glColor3f(1.0, 0.0, 0.0);
+
+	glBegin(GL_LINES);
+	
+	float us = 0.1f;
+	float vs = 0.1f;
+	for (float u=-6.0f;u<6.0f;u+=us)
+	{
+		for (float v=-6.0f;v<6.0f;v+=vs)
+		{
+			float y = -1.0f;
+			glVertex3f(30.0f*u, y, 2.0 + 30.0f * v);
+			glVertex3f(30.0f*(u+us), y, 2.0 + 30.0f * v);
+			glVertex3f(30.0f*u, y, 2.0 + 30.0f * v);
+			glVertex3f(30.0f*u, y, 2.0 + 30.0f * (v + vs));
+		}
+	}
+	
+	glEnd();
+	
 	kosmos::render::end();
 
 	if (liveupdate)
