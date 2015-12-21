1f099ef867451080a3e61fbb50396aeff777e0e9
Dan Nilsson <raxptor@gmail.com>
2015-12-21 13:58:22 +0100

    junk

--- src/terrain/mapstreamer.cpp
+++ src/terrain/mapstreamer.cpp
@@ -15,6 +15,49 @@ namespace mapstreamer
 		GLuint heightmap;
 	};
 
+	// Generate per
+	float noise(uint32_t x, uint32_t y)
+	{
+		uint32_t n = x + y * 57;
+		n = (n << 13) ^ n;
+		return ( 1.0 - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+	}
+
+	float perlin(int octaves, float base, float exp, float s, float t)
+	{
+		float f = 1.0f / base;
+		float out = 0;
+		float a = 1.0f;
+		for (int i=0;i<octaves;i++)
+		{
+			float ls = f * s;
+			float lt = f * t;
+			int is = (int)ls;
+			int it = (int)lt;
+			float fs = ls - is;
+			float ft = lt - it;
+
+			float s00 = noise(is, it);
+			float s01 = noise(is, it + 1);
+			float s10 = noise(is + 1, it);
+			float s11 = noise(is + 1, it + 1);
+
+			float u = s00 + fs * (s10-s00);
+			float v = s01 + fs * (s11-s01);
+			float p = u + ft * (v - u);
+
+			out += a * p;
+			a = exp * a;
+			f = 2.0f * f;
+		}
+		return out;
+	}
+
+	float generate(float s, float t)
+	{
+		return 10.0f * perlin(2, 256, 0.80f, s, t);
+	}
+
 	data* create(int dimensions)
 	{
 		data *d = new data();
@@ -25,7 +68,7 @@ namespace mapstreamer
 		{
 			for (uint32_t v=0;v!=d->heightmap_size;v++)
 			{
-				d->height_data[v * d->heightmap_size + u] = (rand() % 100) * 0.01f; //sample(d, u, v);
+				d->height_data[v * d->heightmap_size + u] = sample(d, u, v);
 			}
 		}
 
@@ -38,7 +81,7 @@ namespace mapstreamer
 
 	float sample(data *d, int u, int v)
 	{
-		return 10.0f * sin(u * 0.03f) + 10.0f * cos(v * 0.05);
+		return generate(u, v);
 	}
 
 	void free(data *d)
